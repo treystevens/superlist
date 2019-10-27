86a069a50fc8420b713581baf537b91989f17306
treykris <trey10027@gmail.com>
2019-10-27 14:22:48 -0400

    Updated view to handle post request and updated tests

--- lists/views.py
+++ lists/views.py
@@ -8,5 +8,15 @@ from django.shortcuts import render
 # Create your views here
 
 
+# def home_page(request):
+#     if request.method == 'POST':
+#         return render(request, 'home.html', {
+#             'new_item_text': request.POST['item_text'],
+#         })
+#     return render(request, 'home.html')
+
+
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
