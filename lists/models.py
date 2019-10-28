9b7d5e595d5250520931832d85997cebb00cf819
treykris <trey10027@gmail.com>
2019-10-28 19:21:26 -0400

    adjust model so that items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here
 
@@ -16,5 +16,6 @@ def view_list(request, slug):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
