a96e567d1a20eaf9fae2a472a9189c20f72e0e9e
treykris <trey10027@gmail.com>
2019-10-29 02:31:58 -0400

    new URL + view for adding to existing lists. FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -69,7 +69,6 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-        self.fail('Finish the test!')
         # She visits that URL - her to-do list is still there.
         # Satisfied, she goes back to sleep
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('lists/new',
          views.new_list, name='new_list'),
-    path('lists/<slug:slug>/',
+    path('lists/<int:list_id>/',
          views.view_list, name='view_list'),
+    path('lists/<int:list_id>/add_item',
+         views.add_item, name='add_item'),
 ]
